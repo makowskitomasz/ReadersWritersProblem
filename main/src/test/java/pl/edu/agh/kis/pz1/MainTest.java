ea330fe76fc38dca8d0b69a29f1b85694898f268
Tomasz Makowski <t.makowski02@gmail.com>
2022-12-19 16:40:49 +0100

    Tests and documentary and SonarQube raport

--- main/src/test/java/pl/edu/agh/kis/pz1/MainTest.java
+++ main/src/test/java/pl/edu/agh/kis/pz1/MainTest.java
@@ -1,12 +1,19 @@
 package pl.edu.agh.kis.pz1;
-
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 import org.junit.Test;
+import pl.edu.agh.kis.pz1.util.Library;
+import pl.edu.agh.kis.pz1.util.Reader;
+import pl.edu.agh.kis.pz1.util.Writer;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.*;
 
 
 public class MainTest {
 
-
+    Random random = new Random();
     /**
      * Test for the construction of Main and the 
      * main method being called
@@ -15,6 +22,49 @@ public class MainTest {
     public void shouldCreateMainObject(){
         Main main = new Main();
         assertNotNull("Main method called.", main);
+    }
+
+    /**
+     * Test to check if as many as number of readers + writers threads have been started
+     */
+    @Test
+    public void howManyThreads(){
+        int numberOfThreads = Thread.activeCount();
+        int numberOfReaders = random.nextInt(10);
+        int numberOfWriters = random.nextInt(10);
+        List<Thread> Readers = new ArrayList<>();
+        List<Thread> Writers = new ArrayList<>();
+        Queue<Thread> queue = new LinkedList<>();
+        Library library = new Library(queue);
+        for (int i = 0; i < Math.max(numberOfReaders, numberOfWriters); i++) {
+            if(i < numberOfReaders) Readers.add(new Thread(new Reader(library, "Reader_" + i), "Reader_" + i));
+            if(i < numberOfWriters) Writers.add(new Thread(new Writer(library, "Writer_" + i), "Writer_" + i));
+        }
+        for(Thread thread : Writers){
+            thread.start();
+        }
+        for(Thread thread : Readers){
+            thread.start();
+        }
+        assertEquals(numberOfReaders + numberOfWriters + numberOfThreads, Thread.activeCount() );
+    }
+
+    /**
+     * Test that checks created arrays
+     */
+    @Test
+    public void arraysCreatedCorrectly() {
+        List<Thread> Readers = new ArrayList<>();
+        List<Thread> Writers = new ArrayList<>();
+        Queue<Thread> queue = new LinkedList<>();
+        Library library = new Library(queue);
+        int numberOfReaders = random.nextInt(10), numberOfWriters = random.nextInt(10);
+        for (int i = 0; i < Math.max(numberOfReaders, numberOfWriters); i++) {
+            if(i < numberOfReaders) Readers.add(new Thread(new Reader(library, "Reader_" + i), "Reader_" + i));
+            if(i < numberOfWriters) Writers.add(new Thread(new Writer(library, "Writer_" + i), "Writer_" + i));
+        }
+        assertEquals(Readers.size(), numberOfReaders);
+        assertEquals(Writers.size(), numberOfWriters);
     }
 }
 
--- utils/src/test/java/WriterTest.java
+++ utils/src/test/java/WriterTest.java
@@ -1,5 +1,4 @@
 import org.junit.Test;
-import org.mockito.Mockito;
 import pl.edu.agh.kis.pz1.util.Library;
 import pl.edu.agh.kis.pz1.util.Writer;
 
@@ -9,14 +8,12 @@ import java.util.List;
 import java.util.Queue;
 
 import static junit.framework.TestCase.assertNotNull;
-import static org.mockito.Mockito.atLeastOnce;
+
 
 
 public class WriterTest {
     Queue<Thread> queue = new LinkedList<>();
     Library library = new Library(queue);
-
-
     /**
      * Test for the construction of Writer and the
      * writer object being called
@@ -24,27 +21,25 @@ public class WriterTest {
     @Test
     public void shouldCreateWriterObject(){
         Writer writer = new Writer(library, "writer_");
-
         assertNotNull("Writer object called.", writer);
     }
 
+    /**
+     * Test for checking if in a 5 seconds writer will stop writing
+     * @throws InterruptedException
+     */
     @Test
     public void writerStopsWriting() throws InterruptedException {
         Queue<Thread> queue = new LinkedList<>();
         Library library1 = new Library(queue);
         int numberOfWriters = 1;
         List<Thread> Writers = new ArrayList<>();
-
         for (int i = 0; i < numberOfWriters; i++) {
             Writers.add(new Thread(new Writer(library1, "writer_" + i)));
         }
-
         for(Thread thread : Writers){
             thread.start();
         }
-
         Thread.sleep(5000);
-
-//        Mockito.verify(library1, atLeastOnce()).stopWriting();
     }
 }
